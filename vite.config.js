a67ec8f0ef21e84422b5ed4c7f5f2f25d131711c
sudolink <sudolink@protonmail.com>
2022-09-28 23:36:30 +0200

    more configing

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BACKEND_URL,
+  //base: process.env.VITE_BACKEND_URL,
   plugins: [react()],
   server: {
     proxy: {
